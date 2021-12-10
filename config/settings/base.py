35f705fce77e84154d462fb20f4ff4f0b2ecf2f3
artem.vasin <artem.vasin>
2021-12-11 00:38:35 +0300

    new: ci

--- config/settings/base.py
+++ config/settings/base.py
@@ -224,7 +224,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "django_magnificent_messages.context_processors.django_magnificent_messages",
                 "read_comics.utils.context_processors.settings_context",
-                "read_comics.users.context_processors.email_verified_context",
+
             ],
         },
     }
