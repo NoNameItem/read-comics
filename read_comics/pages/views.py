71b5dcf432b8bcc13e60f23181a47aae46766bdd
artem.vasin <artem.vasin>
2021-12-15 02:59:00 +0300

    new: Added info about last load on home page

--- read_comics/pages/views.py
+++ read_comics/pages/views.py
@@ -1,8 +1,12 @@
+import datetime
 from typing import Any, Dict
 
+from django.db.models import DateTimeField, Max
+from django.db.models.functions import Trunc
 from django.views.generic import TemplateView
 from utils.comicvine_stats import get_matched_stats
 
+from read_comics.issues.models import Issue
 from read_comics.missing_issues.models import MissingIssue
 
 
@@ -13,6 +17,21 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['matched_stats'] = get_matched_stats()
         context['missing_issues_count'] = MissingIssue.objects.all().count()
+
+        last_update_day = Issue.objects.aggregate(
+            max_day=Max(Trunc('created_dt', 'day', output_field=DateTimeField()))
+        )['max_day']
+        new_issues = Issue.objects.matched().filter(
+            created_dt__gte=last_update_day, created_dt__lt=last_update_day + datetime.timedelta(days=1)
+        ).order_by(
+            'volume__publisher', 'volume', 'numerical_number', 'number'
+        ).select_related(
+            'volume', 'volume__publisher'
+        )
+        context['last_update_day'] = last_update_day
+        context['new_issues'] = new_issues
+        context['new_issues_count'] = new_issues.count()
+
         return context
 
 
