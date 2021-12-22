2126c18affc22c510b3f7a1ee3c1e0167d214e7e
artem.vasin <artem.vasin>
2021-12-22 16:47:07 +0300

    new: robots.txt

--- read_comics/pages/urls.py
+++ read_comics/pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import home_view
 
 app_name = "pages"
 urlpatterns = [
     path("", view=home_view, name="home"),
+    path("robots.txt", TemplateView.as_view(template_name="pages/robots.txt", content_type="text/plain")),
 ]
