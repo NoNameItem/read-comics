f96cf74dbb25f7b5acc0609e648dc23d5263f9ea
artem.vasin <artem.vasin>
2021-12-23 17:25:04 +0300

    fix: Missing issue countes now update after skip/ignore w/o page refresh

--- read_comics/missing_issues/static/missing_issues/js/missing_issues.js
+++ read_comics/missing_issues/static/missing_issues/js/missing_issues.js
@@ -1,3 +1,27 @@
+function updateBadges() {
+  let allCount = $('#all-missing-count').val();
+  let watchedCount = $('#watched-missing-count').val();
+  let thisCount = $('#this-missing-count').val();
+
+  if (allCount === "0") {
+    $('#total-missing-count-badge').remove();
+  } else {
+    $('#total-missing-count-badge').text(allCount);
+  }
+
+  if (thisCount === "0") {
+    $('#this-missing-count-badge').remove();
+  } else {
+    $('#this-missing-count-badge').text(thisCount);
+  }
+
+  if (watchedCount === "0") {
+    $('#watched-missing-count-badge').remove();
+  } else {
+    $('#watched-missing-count-badge .badge').text(watchedCount);
+  }
+}
+
 function initButtons() {
   $('.btn-skip, .btn-ignore').click(function (e) {
     let url = $(this).attr('data-href');
@@ -8,6 +32,7 @@ function initButtons() {
       type        : 'GET'
     }).done(function (data, textStatus, jqXHR) {
       $('#table-wrapper').html(data);
+      updateBadges();
       initButtons();
       initCopy();
       $('[data-toggle="tooltip"]').tooltip();
