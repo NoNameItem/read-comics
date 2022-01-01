663f9f415cdbe680e204a85b9de34fc6063342b1
artem.vasin <artem.vasin>
2022-01-01 22:47:38 +0300

    chg: Changed person download zip structure

--- read_comics/people/views.py
+++ read_comics/people/views.py
@@ -1,5 +1,3 @@
-import math
-
 from django.db.models import Count, Sum
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -182,35 +180,8 @@ person_issue_detail_view = PersonIssueDetailView.as_view()
 
 @logging.methods_logged(logger, ['get', ])
 class PersonDownloadView(BaseZipDownloadView):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.person = None
-
-    def get_files(self):
-        self.person = get_object_or_404(Person, slug=self.kwargs.get('slug'))
-        q = sublist_querysets.get_issues_queryset(self.person)
-        issues_count = q.count()
-
-        if issues_count:
-            num_length = math.ceil(math.log10(q.count()))
-
-            files = [
-                (
-                    self.escape_file_name(
-                        f"{str(num).rjust(num_length, '0')} - {x.volume.name} #{x.number} {x.name}".rstrip(' ')
-                        + x.space_key[-4:]
-                    ),
-                    x.download_link
-                )
-                for num, x in enumerate(q, 1)
-            ]
-        else:
-            files = []
-
-        return files
-
-    def get_zip_name(self):
-        return self.escape_file_name(f"{self.person.name}".replace('\t', '').replace('\n', ''))
+    sublist_querysets = sublist_querysets
+    base_model = Person
 
 
 person_download_view = PersonDownloadView.as_view()
