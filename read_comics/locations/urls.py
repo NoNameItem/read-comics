45988cd2372d02691213bf6308e7b52ada2097d5
artem.vasin <artem.vasin>
2022-01-03 06:45:14 +0300

    fix: Fixed url to issue from location page

--- read_comics/locations/urls.py
+++ read_comics/locations/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path("<str:slug>/stop_watch/", view=stop_watch_view, name="stop_watch"),
     path("<str:slug>/issues/", view=location_issues_list_view, name="issues"),
     path("<str:slug>/volumes/", view=location_volumes_list_view, name="volumes"),
-    path("<str:location_slug>/issues<str:issue_slug>/", view=location_issue_detail_view, name="issue_detail"),
+    path("<str:location_slug>/issues/<str:issue_slug>/", view=location_issue_detail_view, name="issue_detail"),
     path("<str:slug>/download/", view=location_download_view, name="download"),
 ]
