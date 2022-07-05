f526ba13be94137223929c0be8d7306a5947dc55
artem.vasin <artem.vasin>
2022-07-05 17:58:34 +0300

    chg: set console log handler level to INFO

--- config/settings/production.py
+++ config/settings/production.py
@@ -205,7 +205,7 @@ LOGGING = {
     },
     "handlers": {
         "console": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.StreamHandler",
             "formatter": "verbose",
         }
