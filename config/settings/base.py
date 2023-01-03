50ab626628b65b6953fe93181e164a2615ddc6f3
artem.vasin <artem.vasin>
2023-01-03 05:50:36 +0300

    fix: admin email

--- config/settings/base.py
+++ config/settings/base.py
@@ -271,7 +271,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Artem Vasin""", "nonameit@ya.ru")]
+ADMINS = []
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
