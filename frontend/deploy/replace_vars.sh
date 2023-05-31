9f6a0b7b520b47eb84491527e46994ab601849ee
artem.vasin <artem.vasin>
2023-05-31 19:24:43 +0300

    build(frontend): env vars

--- frontend/deploy/replace_vars.sh
+++ frontend/deploy/replace_vars.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 # shellcheck disable=SC2155
+echo "replacing vars"
 export EXISTING_VARS=$(awk 'BEGIN {for (k in ENVIRON) { print k }}' | sed 's/^/\$/g' | paste -sd,)
 file=/usr/share/nginx/html/envs.js
-cat $file | envsubst $EXISTING_VARS > $file
+echo $EXISTING_VARS
+cat $file | envsubst $EXISTING_VARS | tee $file
 cat $file
