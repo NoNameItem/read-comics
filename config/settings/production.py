aedf2eae2350c4b53412b9f49a28a2f40f490f11
artem.vasin <nonameitem@me.com>
2025-01-12 02:56:56 +0300

    fix: moved gunicorn args to env variable

--- config/settings/production.py
+++ config/settings/production.py
@@ -144,11 +144,11 @@ TEMPLATES[0]["OPTIONS"]["context_processors"] += [  # noqa F405
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-DEFAULT_FROM_EMAIL = env("DJANGO_DEFAULT_FROM_EMAIL", default="ReadComics.net <noreply@read-comics.net>")
+DEFAULT_FROM_EMAIL = env("DJANGO_DEFAULT_FROM_EMAIL", default="ReadComics <noreply@read-comics>")
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[ReadComics.net]")
+EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[ReadComics]")
 
 # ADMIN
 # ------------------------------------------------------------------------------
