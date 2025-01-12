16f4080e8fd79d2260a4ad3ef0e81ef8719399ab
artem.vasin <nonameitem@me.com>
2025-01-13 01:51:32 +0300

    fix(spiders): spider delay setting from env

--- read_comics/spiders/settings.py
+++ read_comics/spiders/settings.py
@@ -32,12 +32,12 @@ USER_AGENT = "comicvine_crawler read-comics.net"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 5
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = env.int("SCRAPPY_DOWNLOAD_DELAY", default=3)
 RANDOMIZE_DOWNLOAD_DELAY = False
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
