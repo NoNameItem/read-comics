6b8702fb509b9b34a7d96446f7693fee49c646d5
artem.vasin <nonameitem@me.com>
2025-01-13 00:58:45 +0300

    fix(spiders): comicvine spiders settings

--- read_comics/spiders/settings.py
+++ read_comics/spiders/settings.py
@@ -32,15 +32,15 @@ USER_AGENT = "comicvine_crawler read-comics.net"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 5
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 10
 RANDOMIZE_DOWNLOAD_DELAY = False
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
