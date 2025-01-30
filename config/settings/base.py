1b32e50692343e8041488158830df325741569f4
artem.vasin <nonameitem@me.com>
2025-01-30 13:24:22 +0300

    build: switch celery to rabbitmq and mongo

--- config/settings/base.py
+++ config/settings/base.py
@@ -290,7 +290,11 @@ if USE_TZ:
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
 CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
-CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default=CELERY_BROKER_URL)
+CELERY_MONGODB_BACKEND_SETTINGS = {
+    "database": "read_comics",
+    "authSource": "read_comics",
+}
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
 CELERY_ACCEPT_CONTENT = ["json"]
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
