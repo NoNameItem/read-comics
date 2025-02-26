0f6b90a3a261a014ae3c17707603c549674cc7fb
artem.vasin <nonameitem@me.com>
2025-02-27 01:33:46 +0300

    feat(spiders): save associated_images for issues

--- read_comics/spiders/spiders/issues_spider.py
+++ read_comics/spiders/spiders/issues_spider.py
@@ -6,7 +6,7 @@ class IssuesSpider(BaseSpider):
     LIST_URL_PATTERN = (
         "https://comicvine.gamespot.com/api/issues/?"
         "format=json&"
-        "field_list=id,api_detail_url,site_detail_url,name,aliases,deck,image,issue_number,cover_date,"
+        "field_list=id,api_detail_url,site_detail_url,name,aliases,deck,image,issue_number,cover_date,associated_images"
         "store_date,volume&"
         "sort=id:asc&"
         "offset={offset}&"
@@ -17,5 +17,5 @@ class IssuesSpider(BaseSpider):
     DETAIL_FIELD_LIST = (
         "id,api_detail_url,site_detail_url,name,aliases,deck,description,image,issue_number,"
         "cover_date,store_date,character_credits,character_died_in,concept_credits,location_credits,"
-        "object_credits,person_credits,story_arc_credits,team_credits,team_disbanded_in,volume"
+        "object_credits,person_credits,story_arc_credits,team_credits,team_disbanded_in,volume,associated_images"
     )
