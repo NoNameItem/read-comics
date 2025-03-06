8a852c79a31d0c8f84c15a64a00bff87359f6f05
artem.vasin <nonameitem@me.com>
2025-03-06 23:28:09 +0300

    feat(core): clear selected queues on celery launch

--- read_comics/missing_issues/management/commands/clearqueue.py
+++ read_comics/missing_issues/management/commands/clearqueue.py
@@ -4,5 +4,15 @@ from ...models import APIQueue
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("endpoints", nargs="+", type=str)
+
     def handle(self, *args, **options):
-        APIQueue.objects.all().delete()
+        if len(options["endpoints"]) == 0:
+            APIQueue.objects.all().delete()
+            self.stdout.write(self.style.SUCCESS("Cleared all queues"))
+            return
+
+        for endpoint in options["endpoints"]:
+            APIQueue.objects.filter(endpoints=endpoint).delete()
+            self.stdout.write(self.style.SUCCESS(f"Cleared {endpoint} queue"))
