123b2a48cbc3954f49058dc2a063739771556a67
Artem Vasin <nonameitem@me.com>
2025-03-19 23:28:38 +0300

    chore(main): release 1.23.13

--- read_comics/__init__.py
+++ read_comics/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "1.23.12"  # x-release-please-version
+__version__ = "1.23.13"  # x-release-please-version
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace("-", ".", 1).split(".")])
